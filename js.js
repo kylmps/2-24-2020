6d08e8de12007c3f487851d2a5e7d626acadc1ab
ielbusmente <60253007+ielbusmente@users.noreply.github.com>
2020-02-24 12:25:37 +0800

    pers commit

---
+++ js.js
@@ -0,0 +1,5 @@
+//1. using object literal samples
+
+let person = {name:"gabbi garcia", gender:"female",age:25};
+
+console.log(`${person.name} is a ${person.gender} person whose age is ${person.age}`);
